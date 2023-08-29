003248e71005278fcaa5bb90b8b43256d1c481a0
Mamba1099 <mambakaranja240@gmail.com>
2023-08-29 08:36:36 +0300

    changing data in column

--- tables.sql
+++ tables.sql
@@ -1,10 +1,10 @@
 -- creating database table
 CREATE TABLE my_info (
-    first_name VARCHAR(255),
-    id_numb int,
-    age int,
-    nationality VARCHAR9(255),
-    city    VARCHAR(255)
+    first_name VARCHAR(255) NOT NULL,
+    id_number int NOT NULL,
+    age int NOT NULL,
+    nationality VARCHAR9(255) NOT NULL,
+    city    VARCHAR(255) NOT NULL
 
 
 );
@@ -21,6 +21,14 @@ ADD Email VARCHAR(255);
 -- deleting colmn in a table
 ALTER TABLE my_info
 DROP COLUMN Email;
+
+-- renaming a column in the db
+ALTER TABLE my_info
+RENAME COLUMN city to state;
+
+--changing data type of a column
+ALTER TABLE my_info
+MODIFY COLUMN nationality kenyan;
 
 
 
