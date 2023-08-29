edfdbe48ff7f130cef3654f254cc001566605295
Mamba1099 <mambakaranja240@gmail.com>
2023-08-29 10:16:23 +0300

    using primary key constraints on multiple column

--- constraints.sql
+++ constraints.sql
@@ -1,8 +1,17 @@
 -- using constraints on multiple column
 CREATE TABLE my_info (
-    first_name VARCHAR(255) NOT NULL,
     iD int NOT NULL,
-    username int NOT NULL,
+    username int NOT NULL, 
+    first_name VARCHAR(255) NOT NULL,
     nationality VARCHAR9(255) NOT NULL,
     city    VARCHAR(255) NOT NULL
     CONSTRAINT UC_my_info(ID, username)
+
+-- using primary key constraints on multiple column
+
+CREATE TABLE my_info (
+    iD int NOT NULL PRIMARY KEY,
+    username int NOT NULL PRIMARY KEY, 
+    first_name VARCHAR(255) NOT NULL,
+    nationality VARCHAR9(255) NOT NULL,
+    city    VARCHAR(255) NOT NULL
