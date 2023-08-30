0ac1ce02e7cf2aa36bf21e6799b96c7ed962d444
Mamba1099 <mambakaranja240@gmail.com>
2023-08-30 13:25:45 +0300

    altering table and setting a default value

--- default.sql
+++ default.sql
@@ -10,3 +10,25 @@ CREATE TABLE Persons (
 -- | ID        | int          | NO   |     | NULL    |       |
 -- | City      | varchar(255) | YES  |     | Sandnes |       |
 -- +-----------+--------------+------+-----+---------+-------+
+
+-- altering table and setting default value
+CREATE TABLE info (
+    ID int NOT NULL,
+    City varchar(255)
+);
+
+-- +-------+--------------+------+-----+---------+-------+
+-- | Field | Type         | Null | Key | Default | Extra |
+-- +-------+--------------+------+-----+---------+-------+
+-- | ID    | int          | NO   |     | NULL    |       |
+-- | City  | varchar(255) | YES  |     | NULL    |       |
+-- +-------+--------------+------+-----+---------+-------+
+ALTER TABLE info
+ALTER city SET DEFAULT 'Nairobi';
+
+-- +-------+--------------+------+-----+---------+-------+
+-- | Field | Type         | Null | Key | Default | Extra |
+-- +-------+--------------+------+-----+---------+-------+
+-- | ID    | int          | NO   |     | NULL    |       |
+-- | City  | varchar(255) | YES  |     | Nairobi |       |
+-- +-------+--------------+------+-----+---------+-------+
