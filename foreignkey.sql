bd02b6dd18eaec397f4a0acdaa31298b6b66ddf5
Mamba1099 <mambakaranja240@gmail.com>
2023-08-30 13:10:33 +0300

    altering table and adding foreign key

--- foreignkey.sql
+++ foreignkey.sql
@@ -11,11 +11,19 @@ CREATE TABLE Offices (
     room int NOT NULL
 );
 
+
 ALTER TABLE employee
 RENAME COLUMN ID to employee_id;
 
 ALTER TABLE Offices
 RENAME COLUMN ID to Offices_id;
+
+-- +------------+------+------+-----+---------+----------------+
+-- | Field      | Type | Null | Key | Default | Extra          |
+-- +------------+------+------+-----+---------+----------------+
+-- | Offices_id | int  | NO   | PRI | NULL    | auto_increment |
+-- | room       | int  | NO   |     | NULL    |                |
+-- +------------+------+------+-----+---------+----------------+
 
 
 
@@ -26,12 +34,34 @@ INSERT INTO employee(first_name, last_name) VALUES ('cate', 'job');
 INSERT INTO employee(first_name, last_name) VALUES ('kevin', 'doe');
 INSERT INTO employee(first_name, last_name) VALUES ('harry', 'moon');
 
+-- +-------------+------------+-----------+
+-- | employee_id | first_name | last_name |
+-- +-------------+------------+-----------+
+-- |           1 | Mark       | John      |
+-- |           2 | jane       | joy       |
+-- |           3 | cate       | job       |
+-- |           4 | kevin      | doe       |
+-- |           5 | harry      | moon      |
+-- +-------------+------------+-----------+
+
 INSERT INTO Offices(room) VALUES (100);
 INSERT INTO Offices(room) VALUES (111);
 INSERT INTO Offices(room) VALUES (222);
 INSERT INTO Offices(room) VALUES (333);
 INSERT INTO Offices(room) VALUES (444);
 INSERT INTO Offices(room) VALUES (555);
+
+-- +------------+------+
+-- | Offices_id | room |
+-- +------------+------+
+-- |          1 |  100 |
+-- |          2 |  111 |
+-- |          3 |  222 |
+-- |          4 |  333 |
+-- |          5 |  444 |
+-- |          6 |  555 |
+-- +------------+------+
+
 
 
 CREATE TABLE employeeOffices(
@@ -42,7 +72,32 @@ CREATE TABLE employeeOffices(
     FOREIGN KEY (office_id) REFERENCES Offices(offices_id)
 );
 
+-- +-------------+------+------+-----+---------+-------+
+-- | Field       | Type | Null | Key | Default | Extra |
+-- +-------------+------+------+-----+---------+-------+
+-- | Id          | int  | NO   | PRI | NULL    |       |
+-- | employee_id | int  | YES  | MUL | NULL    |       |
+-- | office_id   | int  | YES  |     | NULL    |       |
+-- +-------------+------+------+-----+---------+-------+
+
 INSERT INTO employeeOffices(Id, employee_id, Office_id) VALUES (1, 1, 3);
 INSERT INTO employeeOffices(Id, employee_id, Office_id) VALUES (2, 2, 6);
 INSERT INTO employeeOffices(Id, employee_id, Office_id) VALUES (3, 3, 5);
 INSERT INTO employeeOffices(Id, employee_id, Office_id) VALUES (4, 4, 1);
+
+
+-- altering table to set foreign key
+CREATE TABLE employeeOffices(
+    Id int PRIMARY KEY NOT NULL,
+    employee_id int,
+    office_id int
+);
+
+
+ALTER TABLE employeeOffices
+ADD FOREIGN KEY (employee_id) REFERENCES employee(employee_id);
+
+ALTER TABLE employeeOffices
+ADD FOREIGN KEY (office_id) REFERENCES Offices(Offices_id);
+
+
